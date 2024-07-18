16014b2715fe26311c63a22559962804c16f6309
jerk-store <taran.leu@outlook.com>
2024-07-18 16:16:17 -0600

    going to delete webpack and then reinstall

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 
+// import * as dotenv from "dotenv";
+// dotenv.config();
+
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
+    .connect(
+      "mongodb://127.0.0.1:27017/shoppler",
+      // process.env.DB_URI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    )
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
     });
